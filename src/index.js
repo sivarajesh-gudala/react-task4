5343c87c850118cd7e0d148a0e2cb171b24794af
sivarajesh-gudala <sivarajesh480@gmail.com>
2020-10-29 18:16:11 +0530

    smallchanges

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter basename="react-task4">
+  <BrowserRouter>
 
   <React.StrictMode>
     <App />
